62c116e1274de00cf838a5780ad1405078146028
Daniel <66730370+dhartmann1979@users.noreply.github.com>
2024-11-07 20:25:57 +0100

    Update install_n8cq.sh

--- install_n8cq.sh
+++ install_n8cq.sh
@@ -3,6 +3,6 @@
 # N8CQ Installer
 apt update
 apt full-upgrade -y
-apt install git build-essential mc zip unzip p7zip-full btop htop apache2 php php-fpm php-cgi php-json php-opcache php-pdo php-readline php-gd php-pear -y
+apt install git build-essential mc zip unzip p7zip-full btop htop hostapd dnsmasq apache2 php php-fpm php-cgi php-json php-opcache php-pdo php-readline php-gd php-pear -y
 wget https://github.com/JiriSko/amixer-webui/releases/download/v1.0.0-rc/amixer-webui_1.0.0-rc_all.deb
 dpkg -i amixer-webui_1.0.0-rc_all.deb
